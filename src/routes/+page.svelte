2a9b8a349af205f5bb67a387e2a273eb9ce77ef0
light7734 <light7734@tuta.io>
2025-02-16 19:00:40 +0330

    refactor: tidy up draft

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,131 +1,90 @@
-<h1>Software Engineer</h1>
-<!-- Brief
+<h1>Portfolio Under Construction</h1>
+<!-- Brief Bio
 -->
 
-<!-- Contact
-Location: Iran - Tehran Province - Tehran
-
-Call: +________________
-Mail: job@light7734.com
-Mail: light7734@tuta.io
-Message:
-    Telegram: @Light7734
-    Matrix: matrix.org:@light7734
-
-[Download] Resume (pdf)
-[Download] Business Card (pdf)
-
-PS: I may not respond till Saturday (unless you call my phone) since I'm **locked in** on other days.
+<!--OPEN SOURCE PROJECTS -->
+<!-- Brief Dazzle
+I love teaching! I've learned that it's the most effective way to solidify your knowledge and leave
+little to no gaps. Dazzle is the collection of my articles teaching a subject in depth. It delves
+into topics such as rendering, mathematics, guidelines and more. It's just my way of giving back to
+the community!
+-->
+<!-- Gallery 
+-->
+<!-- Technical Details 
+Technology behind Dazzle:
+-->
+<!-- Source Code 
+Check out the source code on any of the official mirrors or on the main self-hosted repo.
+    Self-hosted with <3 using Forgejo (a fork of Gittea)
+    CodeBerg mirror
+    GitLab mirror
+    GitHub mirror
+    <Commit_Tiles/>
 -->
 
-<br />
-<h1>Open Source Projects</h1>
-<ol>
-	<li style="margin-left: 32px">Dazzle</li>
-	<!-- Brief
-	I love teaching! I've learned that it's the most effective way to solidify your knowledge and leave
-	little to no gaps. Dazzle is the collection of my articles teaching a subject in depth. It delves
-	into topics such as rendering, mathematics, guidelines and more. It's just my way of giving back to
-	the community!
-    -->
+<!-- Brief Light
+Light is an awesome bleeding-edge, *all the buzzwords* game-engine...
+-->
+<!-- Gallery 
+    * Insert awesome renders here *
+-->
+<!-- Technical Details 
+Features:
+    MSAA
+    SSAO
+    PBR Lighting
 
-	<!-- Gallery 
-    -->
+Technology behind Light:
+    Languages:
+        C++23, CMake, Shell-script, Lua, Python
+    Graphics APIs:
+        OpenGL, Vulkan, Metal, DirectX12
+    CICD: 
+        Drone, Docker, 
+    
+Prominent vendor libs:
+    EnTT, Glfw3.4, Dear (truly a dear) ImGui, 
 
-	<!-- Technical Details 
-    Technology behind Dazzle:
-    -->
+Check out the source code on any of the official mirrors or on the main self-hosted repo.
+    Self-hosted with <3 using Forgejo (a fork of Gittea)
+    CodeBerg mirror
+    GitLab mirror
+    GitHub mirror
 
-	<!-- Source Code 
-    Check out the source code on any of the official mirrors or on the main self-hosted repo.
-        Self-hosted with <3 using Forgejo (a fork of Gittea)
-        CodeBerg mirror
-        GitLab mirror
-        GitHub mirror
-        <Commit_Tiles/>
-    -->
+    <Commit_Tiles/>
+-->
 
-	<br />
-	<li style="margin-left: 32px">Light</li>
-	<!-- Brief
-	Light is an awesome bleeding-edge, *all the buzzwords* game-engine...
-    -->
+<!-- Brief Bonfire
+You're enjoying its warmth on your eyes! It's my portfolio v2.0 rebuilt from scratch using the rightfully
+loved Svelte framework. 0 uninterresting information. 0 tabs/navigaroins, just scroll down :)
+-->
+<!-- Gallery 
+-->
+<!-- Technical Details 
+Bonfire is powered by:
+    Svelte,
+    Tailwindcss,
+    Nginx
+    Segs
+-->
+<!-- Source Code 
+    Self-hosted with <3 using Forgejo (a fork of Gittea)
+    CodeBerg mirror
+    GitLab mirror
+    GitHub mirror
 
-	<!-- Gallery 
-        * Insert awesome renders here *
-    -->
+    <Commit_Tiles/>
+-->
 
-	<!-- Technical Details 
-    Features:
-        MSAA
-        SSAO
-        PBR Lighting
+<!-- Work Experience -->
+<!-- Brief Bitwyrek -->
+<!-- Details -->
 
-    Technology behind Light:
-        Languages:
-            C++23, CMake, Shell-script, Lua, Python
-        Graphics APIs:
-            OpenGL, Vulkan, Metal, DirectX12
-        CICD: 
-            Drone, Docker, 
-        
-    Prominent vendor libs:
-        EnTT, Glfw3.4, Dear (truly a dear) ImGui, 
+<!-- Brief STG -->
+<!-- Details -->
 
-    Check out the source code on any of the official mirrors or on the main self-hosted repo.
-        Self-hosted with <3 using Forgejo (a fork of Gittea)
-        CodeBerg mirror
-        GitLab mirror
-        GitHub mirror
-
-        <Commit_Tiles/>
-    -->
-
-	<br />
-	<li style="margin-left: ">Bonfire</li>
-	<!-- Brief
-	You're enjoying its warmth on your eyes! It's my portfolio v2.0 rebuilt from scratch using the rightfully
-	loved Svelte framework. 0 uninterresting information. 0 tabs/navigaroins, just scroll down :)
-    -->
-
-	<!-- Gallery 
-    -->
-
-	<!-- Technical Details 
-    Bonfire is powered by:
-        Svelte,
-        Tailwindcss,
-        Nginx
-        Segs
-
-    -->
-
-	<!-- Source Code 
-        Self-hosted with <3 using Forgejo (a fork of Gittea)
-        CodeBerg mirror
-        GitLab mirror
-        GitHub mirror
-
-        <Commit_Tiles/>
-    -->
-</ol>
-
-<br />
-<h1>Work Experience</h1>
-<ol>
-	<li style="margin-left: 32px">Title @ Bitwyre [Mmm YYYY - Mmm YYYY]</li>
-	<!-- Brief -->
-	<!-- Details -->
-
-	<br />
-	<li style="margin-left: 32px">Title @ STG (Systematic Trading Group) [Mmm YYYY - Mmm YYYY]</li>
-	<!-- Brief -->
-	<!-- Details -->
-</ol>
-
-<br />
-<h1>Got A Problem?</h1>
-<!-- Brief
+<!-- Call to action (Got a problem?)
 Let me have a shot at it!
 ...
 -->
