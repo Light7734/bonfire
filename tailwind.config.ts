82c15186af7d38bba637804ecbd208a8f03c3a61
light7734 <light7734@tuta.io>
2026-08-14 14:41:51 +0330

    refactor: minor style changes + changed gruvbox dark red color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,51 +3,51 @@ import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
 
 export default {
-	darkMode: 'class', // important for theme switching
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+    darkMode: 'class', // important for theme switching
+    content: ['./src/**/*.{html,js,svelte,ts}'],
 
-	theme: {
-		extend: {
-			colors: {
-				// Gruvbox Light
-				light: {
-					bg: '#fbf1c7',
-					fg: '#3c3836',
-					primary: '#458588',
-					secondary: '#b16286',
-					accent: '#d79921',
-					neutral: '#a89984',
-					muted: '#7c6f64',
-					red: '#cc241d',
-					green: '#98971a',
-					blue: '#458588',
-					yellow: '#d79921',
-					orange: '#d65d0e',
-					purple: '#b16286',
-					aqua: '#689d6a'
-				},
+    theme: {
+        extend: {
+            colors: {
+                // Gruvbox Light
+                light: {
+                    bg: '#fbf1c7',
+                    fg: '#3c3836',
+                    primary: '#458588',
+                    secondary: '#b16286',
+                    accent: '#d79921',
+                    neutral: '#a89984',
+                    muted: '#7c6f64',
+                    red: '#cc241d',
+                    green: '#98971a',
+                    blue: '#458588',
+                    yellow: '#d79921',
+                    orange: '#d65d0e',
+                    purple: '#b16286',
+                    aqua: '#689d6a'
+                },
 
-				// Gruvbox Dark
-				dark: {
-					bg: '#282828',
-					fg: '#ebdbb2',
-					primary: '#83a598',
-					secondary: '#d3869b',
-					accent: '#fabd2f',
-					neutral: '#928374',
-					muted: '#7c6f64',
-					red: '#fb4934',
-					green: '#b8bb26',
-					blue: '#83a598',
-					yellow: '#fabd2f',
-					orange: '#fe8019',
-					purple: '#d3869b',
-					aqua: '#8ec07c'
-				}
-			}
-		}
-	},
+                // Gruvbox Dark
+                dark: {
+                    bg: '#282828',
+                    fg: '#ebdbb2',
+                    primary: '#83a598',
+                    secondary: '#d3869b',
+                    accent: '#fabd2f',
+                    neutral: '#928374',
+                    muted: '#7c6f64',
+                    red: '#e21a41',
+                    green: '#b8bb26',
+                    blue: '#83a598',
+                    yellow: '#fabd2f',
+                    orange: '#fe8019',
+                    purple: '#d3869b',
+                    aqua: '#8ec07c'
+                }
+            }
+        }
+    },
 
-	plugins: [typography, containerQueries]
+    plugins: [typography, containerQueries]
 } satisfies Config;
 
