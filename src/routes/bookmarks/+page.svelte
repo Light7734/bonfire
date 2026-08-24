d5753f2840564c18176ef8cefb5c8c9318472bfb
light7734 <light7734@tuta.io>
2026-08-24 18:35:52 +0330

    Added in some bookmarks

--- src/routes/bookmarks/+page.svelte
+++ src/routes/bookmarks/+page.svelte
@@ -1,1 +1,42 @@
-<h1 > Hai ^~^ Under construction :D fluffy developers working hard to construct {">;3"}</h1>
+<script lang="ts">
+import Note from "$lib/components/note.svelte"
+</script>
+
+<p> Some extremely helpful and/or interesting resources that I've found throughout the internet! </p>
+
+<Note title='Books & Articles' type='resource' flow='false'>
+    <a href="https://www.amazon.com/Real-Time-Rendering-Fourth-Tomas-Akenine-M%C3%B6ller/dp/1138627003"> <p> Real-Time Rendering, 4th Edition, by  Tomas Akenine-Möller</p> </a>
+    <a href="https://www.amazon.com/GPU-Zen-Advanced-Rendering-Techniques/dp/B0GNZJPVZ4"> <p> GPU Zen 4: Advanced Rendering Techniques, by  Wolfgang Friedrich Engel</p> </a>
+    <a href="https://www.amazon.com/exec/obidos/ASIN/1482250926"> <p> Essential Mathematics for Games and Interactive Applications, by James M. Van Verth </p> </a>
+    <a href="https://raytracing.github.io/books/RayTracingInOneWeekend.html"><p>Ray Tracing in One Weekend (and beyond), by Peter Shirley</p></a>
+    <a href="https://gamephysicsweekend.github.io/"><p>Game Physics in One Weekend (and beyond), by Gregory Hodges</p></a>
+    <a href="https://people.freebsd.org/~lstewart/articles/cpumemory.pdf"><p>{"What every programmer should know about memory"}</p></a>
+    <a href="https://www.amazon.com/Large-Scale-Software-Design-John-Lakos/dp/0201633620"><p>{"Large-Scale C++ Software Design"}</p></a>
+    <br/>
+</Note>
+
+<Note title='YouTube' type='resource' flow='false'>
+    <br/>
+    <a href="https://www.youtube.com/watch?v=fNk_zzaMoSs&list=PLZHQObOWTQDPD3MizzM2xVFitgF8hE_ab"> <p> 3Blues1Brown - Essence of Linear Algebra </p> </a>
+    <a href="https://www.youtube.com/watch?v=WUvTyaaNkzM&list=PLZHQObOWTQDMsr9K-rj53DwVRMYO3t5Yr"> <p> 3Blues1Brown - Essence of Calculus </p> </a>
+    <a href="https://www.youtube.com/watch?v=aircAruvnKk&list=PLZHQObOWTQDNU6R1_67000Dx_ZCJB-3pi&pp=0gcJCf4COCosWNin"> <p> 3Blues1Brown - Neural Networks </p> </a>
+    <a href="https://www.youtube.com/@CppCon"> <p> CppCon </p> </a>
+    <a href="https://www.youtube.com/@GMTK"> <p> Game Maker's Toolkit </p> </a>
+    <a href="https://www.youtube.com/@CGPGrey"> <p> CGPGrey </p> </a>
+    <a href="https://www.youtube.com/@lauriewired"> <p> Laurie Wired </p> </a>
+    <a href="https://www.youtube.com/watch?v=51u9ZgrEThg&list=PLmac3HPrav-9UWt-ahViIZxpyQxJ2wPSH"> <p> Pixel Pete - Pixel Art 101 </p> </a>
+    <a href="https://www.youtube.com/watch?v=7BWr2tlK-4c&list=PLLdxW--S_0h4dlWUpl-TzBp-ulqK3NiM_"> <p> AdamCYounis - Pixel Art Class </p> </a>
+    <br/>
+</Note>
+
+<style>
+
+a:hover{
+        text-decoration: underline;
+}
+
+a{
+    display: block;
+}
+
+</style>
